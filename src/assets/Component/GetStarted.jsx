26c5b81b14ab29a2ca787d256b7de5a0fee7e2eb
Nayem Ahmed <nayemk0087@gmail.com>
2026-03-30 11:21:35 +0600

    added my responsive get started section

--- src/assets/Component/GetStarted.jsx
+++ src/assets/Component/GetStarted.jsx
@@ -8,7 +8,7 @@ const GetStarted = () => {
         <div>
             <h2 className='font-semibold text-5xl text-center'>Get Started in 3 Steps</h2>
             <p className='text-gray-600 text-center pt-3 pb-10'>Start using premium digital tools in minutes, not hours.</p>
-            <div className='max-w-[80%] mx-auto grid grid-cols-3 gap-6 mb-20'>
+            <div className='max-w-[80%] mx-auto grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6 mb-20'>
                 <div className='shadow-2xl flex flex-col text-center p-4'>
                     <button className='flex justify-end'>
                         <a className='bg-[#9514FA] rounded-full py-1 px-2 flex justify-center text-white font-semibold'>01</a>
